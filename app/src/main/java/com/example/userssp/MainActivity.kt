c86bcdbf998a01dd021f7fc4c1fe5091df2413fa
Pedro Molina <pedro.molina@petroprix.com>
2022-11-06 20:39:50 +0100

    terminada clase 106

--- app/src/main/java/com/example/userssp/MainActivity.kt
+++ app/src/main/java/com/example/userssp/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.userssp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.userssp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var linearLayoutManager: RecyclerView.LayoutManager
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         setContentView(R.layout.activity_main)
+        userAdapter = UserAdapter(mutableListOf())
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.recycler.apply {
+            layoutManager = linearLayoutManager
+            adapter = userAdapter
+        }
+
     }
 }
