ccf59d7bb9eeef19ab83eef06b14369634eda128
Pedro Molina <pedro.molina@petroprix.com>
2022-11-06 20:07:50 +0100

    adapter

--- app/src/main/java/com/example/userssp/UserAdapter.kt
+++ app/src/main/java/com/example/userssp/UserAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.userssp
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,6 +9,7 @@ import com.example.userssp.databinding.ItemUserBinding
 
 class UserAdapter(private val users: List<User>):RecyclerView.Adapter<UserAdapter.ViewHolder>() {
 
+    private lateinit var context:Context
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val binding = ItemUserBinding.bind(view)
@@ -14,15 +17,19 @@ class UserAdapter(private val users: List<User>):RecyclerView.Adapter<UserAdapte
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
+        context = parent.context
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user,parent,false)
+        return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val user = users.get(position)
+        with(holder){
+            binding.tvOrder.text = user.id.toString()
+            binding.tvName.text = user.name
+        }
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int  = users.size
 
 }
